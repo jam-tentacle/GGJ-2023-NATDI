4cf156fbd80b8fd3c07e38d988ba67205293ee2f
Alex Tagun <alt@highcore.io>
2023-01-31 21:26:33 +0400

    character animator

---
+++ GGJ-2023-NATDI/Assets/Scripts/CharacterAnimator.cs
@@ -0,0 +1,33 @@
+using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class CharacterAnimator : MonoBehaviour
+{
+    public event Action<Vector3, Quaternion> Move;
+
+    private static readonly int VelocityZ = Animator.StringToHash("Velocity Z");
+    private static readonly int Moving = Animator.StringToHash("Moving");
+
+    private Animator _animator;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    public void SetVelocityZ(float value)
+    {
+        _animator.SetFloat(VelocityZ, value);
+    }
+
+    public void SetMoving(bool value)
+    {
+        _animator.SetBool(Moving, value);
+    }
+
+    private void OnAnimatorMove()
+    {
+        Move?.Invoke(_animator.deltaPosition, _animator.rootRotation);
+    }
+}

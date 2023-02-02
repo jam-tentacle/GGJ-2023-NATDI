2289c5897f754677d70a939f5be75107ce463cdb
vantos3001 <iw.lasgin2011@gmail.com>
2023-02-03 00:47:55 +0400

    fix icon and billboards

---
+++ GGJ-2023-NATDI/Assets/Scripts/Billboard.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    void Update() 
+    {
+        transform.rotation = Camera.main.transform.rotation;
+        //transform.Rotate(Vector3.up * 180f);
+    }
+}

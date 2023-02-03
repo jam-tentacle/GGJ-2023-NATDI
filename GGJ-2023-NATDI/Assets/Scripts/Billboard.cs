dd7f45c6ac5970947470fc4498374c9cb36d259c
vantos3001 <iw.lasgin2011@gmail.com>
2023-02-03 23:47:58 +0400

    add test

--- GGJ-2023-NATDI/Assets/Scripts/Billboard.cs
+++ GGJ-2023-NATDI/Assets/Scripts/Billboard.cs
@@ -1,8 +1,9 @@
+using System;
 using UnityEngine;
 
 public class Billboard : MonoBehaviour
 {
-    void Update() 
+    void Update()
     {
         transform.rotation = Camera.main.transform.rotation;
         //transform.Rotate(Vector3.up * 180f);
---
+++ GGJ-2023-NATDI/Assets/Scripts/TestMushroomerUI.cs
@@ -0,0 +1,18 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class TestMushroomerUI : MonoBehaviour
+{
+    [SerializeField] private Damageable _mushroomer;
+    [SerializeField] private TMP_Text _text;
+
+    private void Update()
+    {
+        _text.text = GetText();
+    }
+
+    private string GetText()
+    {
+        return $"Hp = {_mushroomer.Health}";
+    }
+}

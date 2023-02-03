e427d7b8b08b6bab30a372a6f33730912cdea579
EnterToExit <osmiumsdk@gmail.com>
2023-02-03 23:26:56 +0300

    Enemy spawner update

---
+++ GGJ-2023-NATDI/Assets/Scripts/MushroomerSpawnData.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class MushroomerSpawnData
+{
+    public int MushroomerCount;
+    public float OverallSpawnTime;
+    public float SpawnBetweenMushroomerTime;
+}
--- GGJ-2023-NATDI/Assets/Scripts/MushroomerSpawner.cs
+++ GGJ-2023-NATDI/Assets/Scripts/MushroomerSpawner.cs
@@ -1,18 +1,48 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Serialization;
 
-public class MushroomerSpawner : MonoBehaviour
+public class MushroomerSpawner : Service, IUpdate, IStart
 {
+    [SerializeField] private List<MushroomerSpawnData> _mushroomerSpawnData = new List<MushroomerSpawnData>();
     [SerializeField] private GameObject _gameObject;
-    private float _passedTime;
+    private float _passedTimeBetweenSpawn;
+    private float _passedTimeWave;
+    private int _numberOfEnemy;
+    private int _completedWavesCount;
+    private SpawnPoint _point;
+    private float _currentOverallTime;
 
-    private void FixedUpdate()
+    public void GameUpdate(float delta)
     {
-        _passedTime += Time.deltaTime;
-        if (!(_passedTime >= 5f)) return;
+        _currentOverallTime += Time.deltaTime;
 
-        Instantiate(_gameObject, transform.position, Quaternion.identity);
-        _passedTime = 0f;
+        if (_completedWavesCount == _mushroomerSpawnData.Count) return;
+
+        MushroomerSpawnData currentSpawnData = _mushroomerSpawnData[_completedWavesCount];
+
+        if (_currentOverallTime <= currentSpawnData.OverallSpawnTime) return;
+
+        _passedTimeWave -= Time.deltaTime;
+
+        if (_passedTimeWave > 0f) return;
+
+        _passedTimeWave = currentSpawnData.SpawnBetweenMushroomerTime;
+        Instantiate(_gameObject, _point.transform.position, Quaternion.identity);
+        _numberOfEnemy++;
+
+        if (_numberOfEnemy < currentSpawnData.MushroomerCount) return;
+
+        _completedWavesCount++;
+        _numberOfEnemy = 0;
+        _passedTimeWave = 0;
+    }
+
+    public void GameStart()
+    {
+        _point = FindObjectOfType<SpawnPoint>();
     }
 }

4f028c7de5f86f5b90bbfb662cc45b4a93f41038
Alex Tagun <alt@highcore.io>
2023-02-04 00:33:13 +0400

    spawn mushroom on terrain

--- GGJ-2023-NATDI/Assets/Scripts/SpawnerService.cs
+++ GGJ-2023-NATDI/Assets/Scripts/SpawnerService.cs
@@ -3,6 +3,7 @@ using Random = UnityEngine.Random;
 
 public class SpawnerService : Service, IInject
 {
+    private const int Ground = 1 << 6;
     private CollectionService _collectionService;
     private AssetsCollection _assetsCollection;
 
@@ -14,11 +15,20 @@ public class SpawnerService : Service, IInject
 
     public Mushroom SpawnMushroom(Transform t, float radius)
     {
-        Vector2 pos = Random.insideUnitCircle * radius;
+        Vector2 randomPos = Random.insideUnitCircle * radius;
+        Vector3 localPos = new(randomPos.x, 0, randomPos.y);
         Mushroom mushroom = Instantiate(_assetsCollection.MushroomPrefab, t);
-        mushroom.transform.localPosition = new Vector3(pos.x, 0, pos.y);
-        _collectionService.AddMushroom(mushroom);
+        if (RayCastOnTerrain(t.position + localPos, out RaycastHit hit))
+        {
+            mushroom.transform.position = hit.point;
+        }
+        else
+        {
+            mushroom.transform.localPosition = localPos;
 
+        }
+
+        _collectionService.AddMushroom(mushroom);
         return mushroom;
     }
 
@@ -42,4 +52,10 @@ public class SpawnerService : Service, IInject
 
         return area;
     }
+
+    private bool RayCastOnTerrain(Vector3 position, out RaycastHit hit) => Physics.Raycast(position + Vector3.up * 1000,
+        Vector3.down,
+        out hit,
+        Mathf.Infinity,
+        Ground);
 }
